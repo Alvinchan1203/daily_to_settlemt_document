0002eec4760ae75b3747a1210983735a4f54834a
alvinchan(陳紀燃) <alvinchan@futunn.com>
2026-05-05 15:45:07 +0800

    fix Vercel: export Express app directly, not serverless-http

--- api/[...path].js
+++ api/[...path].js
@@ -1,6 +1,5 @@
 global._LOTSIZE_DATA = require('./lotsize_data.json');
 
-const serverless = require('serverless-http');
 const app = require('../server');
 
-module.exports = serverless(app);
+module.exports = app;
